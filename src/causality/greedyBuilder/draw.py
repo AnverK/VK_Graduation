63e547b77b3d2336882e845b461747ce5a937a1f
Anver Khusainov <hanver12@gmail.com>
2020-06-23 14:42:09 +0300

    Fixed callings in greedy algorithm

--- src/causality/greedyBuilder/draw.py
+++ src/causality/greedyBuilder/draw.py
@@ -1,4 +1,3 @@
-from causality.greedyBuilder.TreeBuilder import TreeBuilder
 from causality.greedyBuilder.ClimbingHills import GraphBuilder
 import utils as utils
 import numpy as np
@@ -23,7 +22,7 @@ def draw_graph(G):
     G.draw('suboptimal_graph.png', prog='dot')
 
 
-short_metrics_p, long_metrics_p = utils.read_data(shift=True)
+short_metrics_p, long_metrics_p = utils.read_data(dataset_name='feed_top_ab_tests_pool_big_dataset.csv', shift=True)
 short_metrics = short_metrics_p[:, :, 0]
 long_metrics = long_metrics_p[:, :, 0]
 
@@ -31,30 +30,3 @@ metrics = np.hstack((long_metrics, short_metrics))
 builder = GraphBuilder()
 G = builder.build_graph(metrics)
 draw_graph(G)
-# nx.write_gpickle(G, 'graph.pckl')
-# G = nx.read_gpickle('graph.pckl')
-
-# G = nx.from_scipy_sparse_matrix(tree)
-# from time import perf_counter
-# start = perf_counter()
-# for i in range(10):
-#     tree = builder.build_tree(metrics)
-#     G = nx.from_scipy_sparse_matrix(tree)
-# end = perf_counter()
-# print(end - start)
-
-# _, n_short_metrics = short_metrics.shape
-# _, n_long_metrics = long_metrics.shape
-#
-# node_labels = {}
-# node_colors = ['g'] * n_short_metrics + ['r'] * n_long_metrics
-#
-# for i in range(n_short_metrics):
-#     node_labels[i] = str(i)
-# for i in range(n_long_metrics):
-#     node_labels[i + n_short_metrics] = str(i)
-#
-# edges, weights = zip(*nx.get_edge_attributes(G, 'weight').items())
-# nx.draw_networkx(G, pos=nx.circular_layout(G), arrows=True, edgelist=edges, edge_color=weights, width=5.0,
-#                  edge_vmin=0, edge_cmap=plt.get_cmap("Blues"), labels=node_labels, node_color=node_colors)
-# plt.show()
