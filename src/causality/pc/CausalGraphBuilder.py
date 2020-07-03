1d27805c694121f47c5517035511fed732d30f48
Anver Khusainov <hanver12@gmail.com>
2020-07-04 02:59:44 +0300

    Revert "Fixed sepsets exception"
    This reverts commit fa32bfb4

--- src/causality/pc/CausalGraphBuilder.py
+++ src/causality/pc/CausalGraphBuilder.py
@@ -101,17 +101,14 @@ class CausalGraphBuilder:
                            verbose=verbose,
                            numCores=self.num_cores)
             self.edges = self._from_r_graph_to_edges(res)
-        try:
-            self.sepset = self.extract_sepsets(res)
-        except Exception:
-            self.sepset = None
+        self.sepset = self.extract_sepsets(res)
 
     def get_edges(self):
         assert self.edges is not None, 'Graph should be built by fit() function'
         return self.edges
 
     def get_sepsets(self):
-        assert self.sepset is not None, 'Separation sets are not calculated in fit() function'
+        assert self.sepset is not None, 'Graph should be built by fit() function'
         return self.sepset
 
     def extract_sepsets(self, res):
--- src/causality/pc/UsageExample.py
+++ src/causality/pc/UsageExample.py
@@ -12,7 +12,7 @@ import utils
 
 # ====================================================================================================================
 
-short_metrics_p, long_metrics_p = utils.read_data(dataset_name='feed_top_ab_tests_pool_big_dataset.csv', shift=True)
+short_metrics_p, long_metrics_p = utils.read_data(dataset_name='feed_top_ab_tests_pool_dataset_new.csv', shift=True)
 short_metrics = short_metrics_p[:, :, 0]
 long_metrics = long_metrics_p[:, :, 0]
 metrics = np.hstack((long_metrics, short_metrics))
@@ -28,7 +28,7 @@ n_long_metrics = long_metrics.shape[1]
 
 p_value = 1e-2
 raw_log_path = os.path.join('pcalg_logs', 'example.log')
-builder = CausalGraphBuilder(algorithm='pc', p_value=p_value, num_cores=1)
+builder = CausalGraphBuilder(algorithm='fci', p_value=p_value, num_cores=1, log_path=raw_log_path)
 builder.fit(metrics)
 
 # ====================================================================================================================
@@ -63,14 +63,14 @@ PagDrawer.draw(builder.get_edges(), metrics.shape[1], graph_title, graph_path, l
 
 # ====================================================================================================================
 
-# parser = LogParser(raw_log_path, p_value=p_value, labels=labels)
-#
-# print(parser.edge_existence(0, 1))
-# print(parser.edge_existence('long_0', 'long_1'))
-#
-# # note, edge_direction is order-dependent!
-# print(parser.edge_direction(4, 0))
-# print(parser.edge_direction('long_0', 'long_1'))
-#
-# fetched_log_path = os.path.join('pcalg_logs', 'fetched_example.log')
-# parser.get_fetched_log(fetched_log_path)
+parser = LogParser(raw_log_path, p_value=p_value, labels=labels)
+
+print(parser.edge_existence(0, 1))
+print(parser.edge_existence('long_0', 'long_1'))
+
+# note, edge_direction is order-dependent!
+print(parser.edge_direction(4, 0))
+print(parser.edge_direction('long_0', 'long_1'))
+
+fetched_log_path = os.path.join('pcalg_logs', 'fetched_example.log')
+parser.get_fetched_log(fetched_log_path)
