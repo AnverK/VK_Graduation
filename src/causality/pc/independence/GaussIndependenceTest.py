c7a183c3f9af7e37f6599aedf8fb5363aa79a830
Anver Khusainov <hanver12@gmail.com>
2020-07-04 03:05:32 +0300

    Fixed bugs in Gauss CIT

--- src/causality/pc/independence/GaussIndependenceTest.py
+++ src/causality/pc/independence/GaussIndependenceTest.py
@@ -1,14 +1,13 @@
-import math
-
 import numpy as np
+from math import sqrt, log1p
 from scipy.stats import norm
 
 
 class GaussConditionalIndepTest:
 
-    def __init__(self, corr):
+    def __init__(self, corr, sample_size):
         self.correlation_matrix = corr
-        self.n = len(corr)
+        self.n = sample_size
 
     def gauss_ci_test(self, i, j, ks):
         C = self.correlation_matrix
@@ -17,11 +16,11 @@ class GaussConditionalIndepTest:
             r = C[i, j]
         elif len(ks) == 1:
             k = ks[0]
-            r = (C[i, j] - C[i, k] * C[j, k]) / math.sqrt((1 - C[j, k] ** 2) * (1 - C[i, k] ** 2))
+            r = (C[i, j] - C[i, k] * C[j, k]) / sqrt((1 - C[j, k] ** 2) * (1 - C[i, k] ** 2))
         else:
             m = C[np.ix_([i] + [j] + ks, [i] + [j] + ks)]
             PM = np.linalg.pinv(m)
-            r = -PM[0, 1] / math.sqrt(PM[0, 0] * PM[1, 1])
-        r = min(cut_at, r)
-        res = math.sqrt(self.n - len(ks) - 3) * .5 * abs(math.log1p((2 * r) / (1 - r)))
+            r = -PM[0, 1] / sqrt(PM[0, 0] * PM[1, 1])
+        r = min(cut_at, abs(r))
+        res = sqrt(self.n - len(ks) - 3) * .5 * log1p((2 * r) / (1 - r))
         return 2 * (norm.sf(res))
